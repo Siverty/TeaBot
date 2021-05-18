4cf18b684cdc627973d914eadabe45f4b7df574c
DESKTOP-MG5K2H9\Justin Schots <justinschots13@gmail.com>
2021-05-18 22:23:22 +0200

    merge

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-const token = ('')
+require('dotenv').config();
+const TOKEN = process.env.TOKEN;
+const TENORKEY = process.env.TENORKEY;
 
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-client.login(token)
+const PREFIX = "-";
+
+const VERSION = ('0.0.1');
+
+client.login(TOKEN);
 
 client.once('ready', () => {
-    console.log(`Teabot is online`)
+    console.log("Teabot is online\nVersion", VERSION)
+});
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+//lol command structure oversight
+    if(command === 'ping'){
+
+    }
 });
