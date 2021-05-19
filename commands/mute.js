140f53a73d0160aeb79618619937bc5a6fe431dc
DESKTOP-MG5K2H9\Justin Schots <justinschots13@gmail.com>
2021-05-19 21:25:31 +0200

    alternative version 0.0.6 docker workspace compromised due to unknown error. this is a try to get it working again.

--- commands/mute.js
+++ commands/mute.js
@@ -3,31 +3,35 @@ module.exports = {
     name: 'mute',
     description: "this is a mute command!",
     execute(message, args) {
-        const TARGET = message.mentions.users.first();
-        if (TARGET) {
+        if (message.member.roles.cache.some(r => r.name === "Mods")) {
+            const TARGET = message.mentions.users.first();
+            if (TARGET) {
 
-            let mainRole = message.guild.roles.cache.find(role => role.name === 'Cool');
-            let muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
+                let mainRole = message.guild.roles.cache.find(role => role.name === 'Cool');
+                let muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
 
-            let memberTarget = message.guild.members.cache.get(TARGET.id);
+                let memberTarget = message.guild.members.cache.get(TARGET.id);
 
-            if (!args[1]) {
+                if (!args[1]) {
+                    memberTarget.roles.remove(mainRole.id);
+                    memberTarget.roles.add(muteRole.id);
+                    message.channel.send(`<@${memberTarget.user.id}> has been muted`);
+                    return
+                }
                 memberTarget.roles.remove(mainRole.id);
                 memberTarget.roles.add(muteRole.id);
-                message.channel.send(`<@${memberTarget.user.id}> has been muted`);
-                return
-            }
-            memberTarget.roles.remove(mainRole.id);
-            memberTarget.roles.add(muteRole.id);
-            message.channel.send(`<@${memberTarget.user.id}> has been muted for ${MS(MS(args[1]))}`);
+                message.channel.send(`<@${memberTarget.user.id}> has been muted for ${MS(MS(args[1]))}`);
 
-            setTimeout(function () {
-                memberTarget.roles.remove(muteRole.id);
-                memberTarget.roles.add(mainRole.id);
-                message.channel.send(`<@${memberTarget.user.id}> has been unmuted.\n${MS(MS(args[1]))} have passed:alarm_clock:`);
-            }, MS(args[1]));
-        } else {
-            message.channel.send('Cant find that member!');
+                setTimeout(function () {
+                    memberTarget.roles.remove(muteRole.id);
+                    memberTarget.roles.add(mainRole.id);
+                    message.channel.send(`<@${memberTarget.user.id}> has been unmuted.\n${MS(MS(args[1]))} have passed:alarm_clock:`);
+                }, MS(args[1]));
+            } else {
+                message.channel.send('Cant find that member!');
+            }
+        }else {
+            message.reply("you don't have the permission to use this command")
         }
     }
 }
--- commands/unmute.js
+++ commands/unmute.js
@@ -2,18 +2,22 @@ module.exports = {
     name: 'unmute',
     description: "This unmutes a member",
     execute(message, args) {
-        const target = message.mentions.users.first();
-        if (target) {
-            let mainRole = message.guild.roles.cache.find(role => role.name === 'Cool');
-            let muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
+        if (message.member.roles.cache.some(r => r.name === "Mods")) {
+            const target = message.mentions.users.first();
+            if (target) {
+                let mainRole = message.guild.roles.cache.find(role => role.name === 'Cool');
+                let muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
 
-            let memberTarget = message.guild.members.cache.get(target.id);
+                let memberTarget = message.guild.members.cache.get(target.id);
 
-            memberTarget.roles.remove(muteRole.id);
-            memberTarget.roles.add(mainRole.id);
-            message.channel.send(`<@${memberTarget.user.id}> has been unmuted`);
+                memberTarget.roles.remove(muteRole.id);
+                memberTarget.roles.add(mainRole.id);
+                message.channel.send(`<@${memberTarget.user.id}> has been unmuted`);
+            } else {
+                message.channel.send('Cant find that member!');
+            }
         } else {
-            message.channel.send('Cant find that member!');
+            message.reply("you don't have the permission to use this command")
         }
     }
 }
