b1ad6a343c88130e65f6bd5b08a6517382b12bae
DESKTOP-MG5K2H9\Justin Schots <justinschots13@gmail.com>
2021-05-20 00:32:14 +0200

    version 0.0.6D: added welcome role and handler, not sure if it is stable. will do more research.

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
+//used for corrospondance between this and the .env file
 require('dotenv').config();
+
+//easy input for the tokens put in the .env file
 const TOKEN = process.env.TOKEN;
 const TENORKEY = process.env.TENORKEY;
 
+//don't change this
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
+//prefix, used to write specific commands
 const PREFIX = "-";
 
 const fs = require('fs');
 
+//makes commands a collection within discord
 client.commands = new Discord.Collection();
 
+//determines where to look for the commands for use in the command handler
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
@@ -19,10 +25,13 @@ for (const file of commandFiles) {
     client.commands.set(command.name, command);
 }
 
-const VERSION = ('0.0.6');
+//handy placeholder for the bot version
+const VERSION = ('0.0.7');
 
+//login for client, defined in the .env file
 client.login(TOKEN);
 
+//under here is the first thing the bot does when booted up
 client.once('ready', () => {
     console.log("Teabot is online\nVersion", VERSION);
     client.user.setActivity("out for a tea party", {
@@ -31,7 +40,15 @@ client.once('ready', () => {
     })
 });
 
+//welcome role
+client.on('guildMemberAdd', guildMember =>{
+    let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'Welcome');
 
+    guildMember.roles.add(welcomeRole);
+    guildMember.guild.channels.cache.get('844677904246898689').send(`Welcome <@${guildMember.user.id}> to our server! Make sure to check out the rules channel!`)
+});
+
+//message handler
 client.on('message', message => {
     if (!message.content.startsWith(PREFIX) || message.author.bot) return;
 
