bc7b2ae0984887efd6a37e0c8bbd17be56f6ed50
DESKTOP-MG5K2H9\Justin Schots <justinschots13@gmail.com>
2021-05-19 22:27:02 +0200

    version 0.0.6C: Fixed an issue that baffled me, turns out I am stupid(const MS --> const ms) fixed mute.js

--- index.js
+++ index.js
@@ -19,16 +19,16 @@ for (const file of commandFiles) {
     client.commands.set(command.name, command);
 }
 
-const VERSION = ('0.0.5');
+const VERSION = ('0.0.6');
 
 client.login(TOKEN);
 
 client.once('ready', () => {
     console.log("Teabot is online\nVersion", VERSION);
-    // client.user.setActivity("out for a tea party", {
-    //     type: "WATCHING",
-    //     url: "https://github.com/Siverty/TeaBot"
-    // })
+    client.user.setActivity("out for a tea party", {
+        type: "WATCHING",
+        url: "https://github.com/Siverty/TeaBot"
+    })
 });
 
 
