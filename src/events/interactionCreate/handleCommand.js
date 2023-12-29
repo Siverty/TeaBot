20456b66be4e0cd9714b00a8549bdbe4d2932c7e
Justin_Home <justinschots13@gmail.com>
2023-12-29 22:54:02 +0100

    grammar

--- src/events/interactionCreate/handleCommand.js
+++ src/events/interactionCreate/handleCommand.js
@@ -26,7 +26,7 @@ module.exports = async (client, interaction) => {
     if (commandObject.testOnly) {
       if (!(interaction.guild.id === testServer)) {
         interaction.reply({
-          content: "This command cannot be ran here.",
+          content: "This command cannot be ran on this server.",
           ephemeral: true,
         });
         return;
