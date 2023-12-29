118fa469fdcf1a9e94e591439583b1a42f8c5430
Justin_Home <justinschots13@gmail.com>
2023-12-29 21:28:28 +0100

    made more compatible

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const { Client, IntentsBitField } = require('discord.js');
-const eventHandler = require('./handlers/eventHandler');
+require("dotenv").config();
+const { Client, IntentsBitField } = require("discord.js");
+const eventHandler = require("./handlers/eventHandler");
 
 const client = new Client({
   intents: [
